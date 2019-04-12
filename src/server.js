aba277e9a54e45ce74f4433c9dd884d14e41519c
drd-2rcl <drd.rclrd@gmail.com>
2019-04-11 22:16:10 -0300

    middlewares to req JSON and req files

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/teste', (req, res) => {
-  return res.send('EAE');
-});
+// cadastrar um módulo(middleware) no express (app.use)
+// express.json() => é um middleware que ajuda o servidor a entender as requisições que chegam em formato JSON
+app.use(express.json());
+// express.urlencoded() => permite o envio de arquivos nas requisições
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
 
 app.listen(3333);
