a289f0d60f4040ef417fa3410a32eab8c83e18c4
drd-2rcl <drd.rclrd@gmail.com>
2019-04-11 22:49:46 -0300

    connect to ORM

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://drd-2rcl:drd-2rcl@cluster0-w3p1h.mongodb.net/test?retryWrites=true', {
+  useNewUrlParser: true
+});
 
 // cadastrar um módulo(middleware) no express (app.use)
 // express.json() => é um middleware que ajuda o servidor a entender as requisições que chegam em formato JSON
