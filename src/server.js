d3cfd07eade94ef762f3fcbe380bd39929272744
drd-2rcl <drd.rclrd@gmail.com>
2019-04-14 12:26:46 -0300

    create route socket.io for receive real time db

--- src/server.js
+++ src/server.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
-mongoose.connect('mongodb+srv://drd-2rcl:drd-2rcl@cluster0-w3p1h.mongodb.net/test?retryWrites=true', {
-  useNewUrlParser: true
-});
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box);
+  })
+})
+
+mongoose.connect(
+  'mongodb+srv://drd-2rcl:drd-2rcl@cluster0-w3p1h.mongodb.net/test?retryWrites=true', 
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+})
 
 // cadastrar um módulo(middleware) no express (app.use)
 // express.json() => é um middleware que ajuda o servidor a entender as requisições que chegam em formato JSON
 app.use(express.json());
 // express.urlencoded() => permite o envio de arquivos nas requisições
 app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
