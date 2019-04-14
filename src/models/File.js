598bf8d8692c095c2f0289848f660eef2069bf7f
drd-2rcl <drd.rclrd@gmail.com>
2019-04-14 11:20:34 -0300

    fix models file to receive data

--- src/models/File.js
+++ src/models/File.js
@@ -1,17 +1,17 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const File = new mongoose.Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   path: {
-//     type: String,
-//     required: true,
-//   },
-// }, {
-//   // createdAt and updatedAt em cada registro da minha tabela (schema)
-//   timestamps: true
-// });
+const File = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  path: {
+    type: String,
+    required: true,
+  },
+}, {
+  // createdAt and updatedAt em cada registro da minha tabela (schema)
+  timestamps: true
+});
 
-// module.exports = mongoose.model("File", File);
+module.exports = mongoose.model("File", File);
