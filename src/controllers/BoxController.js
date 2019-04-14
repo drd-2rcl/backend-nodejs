59a2fc37b4125574e36516b0ce782bad6cbf62b5
drd-2rcl <drd.rclrd@gmail.com>
2019-04-14 10:57:18 -0300

    fix all

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -2,8 +2,6 @@ const Box = require("../models/Box");
 
 class BoxController {
   async store(req, res) {
-    
-
     const box = await Box.create(req.body);
 
     return res.json(box);
--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,11 +1,19 @@
-const File = require("../models/File");
+// const Box = require("../models/Box");
+// const File = require("../models/File");
 
-class FileController {
-  async store(req, res) {
-    console.log(req.file);
-    // Criar um arquivo
-    return res.send('OK');
-  }
-}
+// class FileController {
+//   async store(req, res) {
+//     const box = await Box.findById(req.params.id);
 
-module.exports = new FileController();
+//     const file = await File.create({
+//       title: req.file.originalname,
+//       path: req.file.key,
+//     });
+
+//     box.files.push(file);
+//     await box.save();
+//     return res.json(file);
+//   }
+// }
+
+// module.exports = new FileController();
